367ccdb8908867a344f7eac8c358ead2796f88e9
dyy222dyy222 <yydai222@outlook.com>
2021-11-22 16:08:42 -0500

    Add anotherrr test

--- test_portfolio.py
+++ test_portfolio.py
@@ -8,3 +8,9 @@ def test_buy_one_stock():
     p = Portfolio()
     p.buy("IBM", 100, 176.48)
     assert p.cost() == 17648.0
+
+def test_buy_one_stock():
+    p = Portfolio()
+    p.buy("IBM", 100, 176.48)
+    p.buy("HPQ", 100, 36.5)
+    assert p.cost() == 21263.0
