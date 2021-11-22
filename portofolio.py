e9789914c85ec23cd7c8b11ae824126ab61c2110
dyy222dyy222 <yydai222@outlook.com>
2021-11-22 11:32:31 -0500

    Add an impletation

---
+++ portofolio.py
@@ -0,0 +1,12 @@
+class Portofolio:
+    def __init__(self):
+        self._stocks = []
+
+    def buy(self, name, shares, price):
+        self._stocks.append((name, shares, price))
+
+    def cost(self):
+        cost = 0
+        for stock in self._stocks:
+            cost += stock[1] * stock[2]
+        return cost
